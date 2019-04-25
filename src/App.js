4ce8fbd4d44b2fbf25396fa19cad8cfaa1715e37
andrew.wu <andrew.wu@tenoz.tw>
2019-04-25 22:51:15 +0800

    overview page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Main.css';
+import './icon/font/css/open-iconic.css'
+import './Open-sans.css'
+import Donut from './component/d3Charts/donut/donut.js'
+import Chart from './component/d3Charts/chart/Chart.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      // <body>
+        <div className="content">
+          <div className="sidenav info-block">
+            <div className="menu">
+              <icon className="oi icon" data-glyph="list"></icon>
+            </div>
+            
+            <div className="category">
+              <icon className="oi icon" data-glyph="pie-chart"></icon>                
+              <icon className="oi icon" data-glyph="contrast"></icon>
+              <icon className="oi icon" data-glyph="folder"></icon>
+            </div>
+            
+          </div>
+          <div className="main-content">
+            <div className="navbar">
+              <div className="page">
+                <div className="head">
+                  <h1>Overview</h1>
+                </div>          
+              </div>
+            </div>
+            <div className="container">
+              <div className="piechart info-block">
+                <div className ="title">
+                  <h3>Device Status</h3>
+                </div>
+                <div className="charts">
+                  <div className='arc-chart' color="FFFD87">
+                    <Donut></Donut>
+                  </div>
+                    <div className='arc-chart' color="#AA525A" >
+                    <Donut></Donut>
+                  </div>
+                </div>
+              </div>
+              <div className="weather info-block">
+                <div className="title">
+                  <h3>Sensor Stats</h3>
+                </div>
+                <div className="charts">
+                  <div className="block">
+                  <Chart></Chart>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      // </body>
+
     );
   }
 }
---
+++ src/component/hi.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export class Template extends Component {
+    render() {
+        return (
+            <div> component </div>
+        );
+    }
+};
+export default Template;
