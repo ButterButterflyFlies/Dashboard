3fa46d154ba97260866c9a7f25fddf634d474c49
Andrew Wu <ppckingdom888@gmail.com>
2020-05-25 16:19:18 +0800

    Updates

---
+++ precache-manifest.e37f5d90ec91016319ac5d255d633176.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "eca2c26759a009a4a9361151efe99e74",
+    "url": "/my-react-dashboard/static/media/open-iconic.eca2c267.svg"
+  },
+  {
+    "revision": "93cc7aa654ae36b0828716f5ec3159cd",
+    "url": "/my-react-dashboard/static/media/open-iconic.93cc7aa6.ttf"
+  },
+  {
+    "revision": "527eff645d8ead16666afb6931041279",
+    "url": "/my-react-dashboard/static/media/open-iconic.527eff64.otf"
+  },
+  {
+    "revision": "47151e87c5a8666791a91007de4962da",
+    "url": "/my-react-dashboard/static/media/open-iconic.47151e87.eot"
+  },
+  {
+    "revision": "3cf97837524dd7445e9d1462e3c4afe2",
+    "url": "/my-react-dashboard/static/media/open-iconic.3cf97837.woff"
+  },
+  {
+    "revision": "9c52e8aa8bb6751de76d",
+    "url": "/my-react-dashboard/static/js/runtime~main.9c52e8aa.js"
+  },
+  {
+    "revision": "d92aa92a25eaa383f6ef",
+    "url": "/my-react-dashboard/static/js/main.d92aa92a.chunk.js"
+  },
+  {
+    "revision": "48d7d7a20bad7f63f1fe",
+    "url": "/my-react-dashboard/static/js/1.48d7d7a2.chunk.js"
+  },
+  {
+    "revision": "d92aa92a25eaa383f6ef",
+    "url": "/my-react-dashboard/static/css/main.76bbf184.chunk.css"
+  },
+  {
+    "revision": "ec11455888dd87d0d7b76b2013d7c691",
+    "url": "/my-react-dashboard/index.html"
+  }
+];
